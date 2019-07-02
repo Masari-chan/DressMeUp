0150fda1d1cbdf368c360fd6989c5e15b7933513
María <al188288@uji.es>
2019-07-02 08:00:08 +0200

    assets/etc

---
+++ Assets/PJController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PJController : MonoBehaviour
+{
+    Vector3 posicionInicial; 
+    [SerializeField]
+    public GameObject character;
+    [SerializeField]
+    GameObject CameraObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicial = CameraObj.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
